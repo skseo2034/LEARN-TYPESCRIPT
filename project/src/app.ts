8d75a5f6db21cf895394e0d668721d829d27eb08
서수교 <sk.seo@navercorp.com>
2022-06-28 09:00:00 +0900

    20220628_학습

--- project/src/app.ts
+++ project/src/app.ts
@@ -57,7 +57,6 @@ function createSpinnerElement(id: string) {
 
 // state
 let isDeathLoading = false;
-const isRecoveredLoading = false;
 
 // api
 function fetchCovidSummary(): Promise<AxiosResponse<CovidSummaryResponse>> {
@@ -198,7 +197,7 @@ async function setupData() {
 	setLastUpdatedTimestamp(data);
 }
 
-function renderChart(data: any, labels: any) {
+function renderChart(data: number[], labels: string[]) {
 	const canvas = $('#lineChart') as HTMLCanvasElement;
 	const ctx = canvas.getContext('2d');
 	Chart.defaults.color = '#f5eaea';
@@ -220,11 +219,13 @@ function renderChart(data: any, labels: any) {
 	});
 }
 
-function setChartData(data: any) {
-	const chartData = data.slice(-14).map((value: any) => value.Cases);
+function setChartData(data: CountrySummaryResponse) {
+	const chartData = data
+		.slice(-14)
+		.map((value: CountrySummaryInfo) => value.Cases);
 	const chartLabel = data
 		.slice(-14)
-		.map((value: any) =>
+		.map((value: CountrySummaryInfo) =>
 			new Date(value.Date).toLocaleDateString().slice(5, -1)
 		);
 	renderChart(chartData, chartLabel);
