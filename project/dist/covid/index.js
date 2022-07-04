dfdd84f10664dd0be05527e4afbda22ac5430b0b
서수교 <sk.seo@navercorp.com>
2022-07-04 15:14:47 +0900

    index.js commit

---
+++ project/dist/covid/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CovidStauts = void 0;
+var CovidStauts;
+(function (CovidStauts) {
+    CovidStauts["Confirmed"] = "confirmed";
+    CovidStauts["Recovered"] = "recovered";
+    CovidStauts["Deaths"] = "deaths";
+})(CovidStauts = exports.CovidStauts || (exports.CovidStauts = {}));
